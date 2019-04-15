267ab74a86e3502cbd8a5b3c3baa51bc58599f1d
joanaluka <47768918+joanaluka@users.noreply.github.com>
2019-04-15 17:32:07 +0300

    Add files via upload

---
+++ hello/calculator.py
@@ -0,0 +1,28 @@
+def add(a,b):
+	answer=a+b
+	return answer
+def multiply(a,b):
+	answer=a*b
+	return answer
+
+def subtract(a,b):
+    answer=a-b
+    return answer
+
+def division(a,b):
+    answer=a/b
+    return answer
+
+def whole_division(a,b):
+    answer=a//b
+    return answer
+
+def modulus(a,b): 
+	answer=a%b
+	return answer
+
+def exponent(a,b):
+	answer=a**b
+	return answer	   
+def percentage(a,b):
+	answer=(a/b)*100
